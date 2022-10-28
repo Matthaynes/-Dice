d10e87b730c728ef008a98cdce925c6951a09ece
MatthewH18 <90624248+Matthaynes@users.noreply.github.com>
2022-10-28 15:01:17 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var dice = {
+  sides: 6,
+  roll: function () {
+    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+    return randomNumber;
+  }
+}
+function printNumber(number) {
+  var placeholder = document.getElementById('pumber');
+  placeholder.innerHTML = number;
+}
+var button = document.getElementById('button');
+
+button.onclick = function() {
+  var result = dice.roll();
+  printNumber(result);
+};
+
